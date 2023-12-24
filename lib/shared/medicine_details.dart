cd1d219368ba86e22c1ca31c77e01003234ade77
Yousef Razzouk <yousefrazouk6@gmail.com>
2023-12-24 22:42:31 +0300

    hello

---
+++ lib/shared/medicine_details.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MedicineDetails extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return _MedicineDetails();
+  }
+
+}
+
+class _MedicineDetails extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
