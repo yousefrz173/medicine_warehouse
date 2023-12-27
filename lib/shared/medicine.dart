c1ee78dcb3707c1f000f27c10d73b95069717088
hassan.ali <hassan.ali.36900@outlook.com>
2023-12-27 14:02:13 +0300

    refactoring the Medicine Class

--- lib/shared/medicine.dart
+++ lib/shared/medicine.dart
@@ -1,86 +1,44 @@
 class Medicine {
-  late String _scientificName;
-  late String _commercialName;
-  late String _genre;
-  late String _company;
-  late DateTime _expirationDate;
-  late double _price;
-  late int _amount;
-  late int _id;
+  String scientificName;
+  String commercialName;
+  String genre;
+  String company;
+  DateTime expirationDate;
+  double price;
+  int amount;
+  int id;
 
   Medicine({
-    required int id,
-    required String scientificName,
-    required String commercialName,
-    required String genre,
-    required String company,
-    required DateTime expirationDate,
-    required double price,
-    required int amount,
-  })  : _scientificName = scientificName,
-        _commercialName = commercialName,
-        _genre = genre,
-        _company = company,
-        _expirationDate = expirationDate,
-        _price = price,
-        _amount = amount,
-        _id = id ;
+    required int this.id,
+    required String this.scientificName,
+    required String this.commercialName,
+    required String this.genre,
+    required String this.company,
+    required DateTime this.expirationDate,
+    required double this.price,
+    required int this.amount,
+  });
 
-  Map<String, Object> get medicineInfo =>  {
-    "id": _id,
-    "scientificName": _scientificName,
-    "commercialName": _commercialName,
-    "genre": _genre,
-    "company": _company,
-    "expirationDate": _expirationDate,
-    "price": _price,
-  };
+  Map<String, Object> get medicineInfo => {
+        "id": id,
+        "scientificName": scientificName,
+        "commercialName": commercialName,
+        "genre": genre,
+        "company": company,
+        "expirationDate": expirationDate,
+        "price": price,
+      };
 
-  int get amount => _amount;
-
-  set amount(int value) {
-    _amount = value;
+  @override
+  bool operator ==(Object rhs) {
+    if (rhs is Medicine)
+      return ((this.id == rhs.id) &&
+          (this.commercialName == rhs.commercialName) &&
+          (this.scientificName == rhs.scientificName) &&
+          (this.genre == rhs.genre) &&
+          (this.company == rhs.company) &&
+          (this.expirationDate == rhs.expirationDate) &&
+          (this.price == rhs.price));
+    return this == rhs as Medicine;
   }
-
-  int get id => _id;
-
-  set id(int value) {
-    _id = value;
-  }
-
-  set price(double value) {
-    _price = value;
-  }
-
-  set expirationDate(DateTime value) {
-    _expirationDate = value;
-  }
-
-  set company(String value) {
-    _company = value;
-  }
-
-  set genre(String value) {
-    _genre = value;
-  }
-
-  set commercialName(String value) {
-    _commercialName = value;
-  }
-
-  set scientificName(String value) {
-    _scientificName = value;
-  }
-
-  double get price => _price;
-
-  DateTime get expirationDate => _expirationDate;
-
-  String get company => _company;
-
-  String get genre => _genre;
-
-  String get commercialName => _commercialName;
-
-  String get scientificName => _scientificName;
 }
