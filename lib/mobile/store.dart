3725c7c419e8b58df0ea4a302dc5bb606c081484
hassan.ali <hassan.ali.36900@outlook.com>
2023-12-31 10:40:27 +0300

    ok

--- lib/mobile/store.dart
+++ lib/mobile/store.dart
@@ -28,21 +28,27 @@ class _StoreState extends State<Store> {
     loadCategories();
   }
 
+  void _categoryTapped({required String categoryName}) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+          builder: (context) => categoryStore(category: categoryName)),
+    );
+  }
+
   void loadCategories() async {
     categories = await ImportantLists.loadCategories();
     setState(() {
       categoriesWidgets = List.generate(
           categories.length,
           (index) => ListTile(
-                title: Text(categories[index]),
-                tileColor: Colors.purple,
-              ));
+              title: Text(categories[index]),
+              tileColor: Colors.purple,
+              onTap: () => setState(
+                  () => _categoryTapped(categoryName: categories[index]))));
     });
   }
 
-  void _medicinetapped(Medicine medicine) {
-
-  }
+  void _medicinetapped(Medicine medicine) {}
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +59,66 @@ class _StoreState extends State<Store> {
         ),
         body: Column(
           children: categoriesWidgets,
+        ));
+  }
+}
+
+class categoryStore extends StatefulWidget {
+  categoryStore({super.key, required this.category});
+  final String category;
+  @override
+  State<StatefulWidget> createState() =>
+      _categoryStoreState(category: category);
+}
+
+class _categoryStoreState extends State<categoryStore> {
+  String category;
+  late List<Widget> categoryWidgets;
+  late List<Medicine> medicines;
+
+  _categoryStoreState({required this.category});
+
+  late final Widget emptyPage;
+
+  @override
+  void initState() {
+    super.initState();
+    emptyPage = const Text('');
+    categoryWidgets = [emptyPage];
+    _refresh();
+  }
+
+  void _medicineTapped(Medicine medicine) {}
+
+  void loadMedicines() async {
+    medicines = await ImportantLists.loadCategoryMedicines(this.category);
+    loadWidgets();
+
+  }
+
+  void loadWidgets() => setState(() {
+        categoryWidgets = List.generate(
+            medicines.length,
+            (index) => ListTile(
+                  title: Text(medicines[index].commercialName),
+                  tileColor: Colors.purple,
+                  onTap: () => _medicineTapped(medicines[index]),
+                ));
+      });
+
+  void _refresh() {
+    loadMedicines();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: _refresh,
+          child: const Icon(Icons.refresh),
+        ),
+        body: Column(
+          children: categoryWidgets,
         ));
   }
 }
